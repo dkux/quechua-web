e4c3192f64c7aa56aca1b78d2c0bb47bb0ca1063
Daniel Kux <danielkux@gmail.com>
2018-11-03 18:23:25 -0300

    Primer commit

---
+++ src/components/AuthService.js
@@ -0,0 +1,109 @@
+import decode from 'jwt-decode';
+export default class AuthService {
+    constructor(domain) {
+        this.domain = domain || 'http://localhost:8080'
+        this.fetch = this.fetch.bind(this)
+        this.login = this.login.bind(this)
+        this.getProfile = this.getProfile.bind(this)
+    }
+
+    login(username, password) {
+        // Get a token
+        return this.fetch(`${this.domain}/api/authenticate`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            })
+        }).then(res => {
+            this.setToken(res.id_token);
+            return Promise.resolve(this._getUserData());
+        }).catch(err =>{
+            return Promise.reject("Usuario o contraseña invalido");
+        })
+    }
+
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken()
+        return !!token && !this.isTokenExpired(token) // handwaiving here
+    }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            }
+            else
+                return false;
+        }
+        catch (err) {
+            return false;
+        }
+    }
+
+    setToken(idToken) {
+        // Saves user token to sessionStorage
+        console.log(idToken);
+        sessionStorage.setItem('id_token', idToken)
+    }
+
+    getToken() {
+        // Retrieves the user token from sessionStorage
+        return sessionStorage.getItem('id_token')
+    }
+
+    logout() {
+        // Clear user token and profile data from sessionStorage
+        sessionStorage.removeItem('id_token');
+    }
+
+    getProfile() {
+        return JSON.parse(sessionStorage.getItem("user"));
+    }
+
+
+    fetch(url, options) {
+        // performs api calls sending the required authentication headers
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+        if (this.loggedIn()) {
+            headers['Authorization'] = 'Bearer ' + this.getToken()
+        }
+
+        return fetch(url, {
+            headers,
+            ...options
+        })
+            .then(this._checkStatus)
+            .then(response => response.json())
+    }
+
+    _checkStatus(response) {
+        // raises an error in case response status is not a success
+        if (response.status >= 200 && response.status < 300) {
+            return response
+        } else {
+            var error = new Error(response.statusText)
+            error.response = response
+            throw error
+        }
+    }
+
+    _getUserData() {
+        return this.fetch(`${this.domain}/api/account`, {
+            method: 'GET',
+        }).then(res => {
+            if (res.authorities.indexOf('ROLE_ADMIN') < 0) {
+                this.logout();
+                return Promise.reject("No es un usuario Admin");
+            }
+            sessionStorage.setItem("user", JSON.stringify(res));
+            return Promise.resolve(res);
+        });
+    }
+}
---
+++ src/components/Login.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, Input, FormFeedback, Card, CardTitle, CardText, Row, Col, Container, Alert } from 'reactstrap';
+import './Login.css';
+import AuthService from './AuthService';
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state  = {
+            email: '',
+            password: '',
+            formErrors: {email: '', password: ''},
+            emailValid: false,
+            passwordValid: false,
+            formValid: false,
+            globalError:''
+        };
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.Auth = new AuthService();
+    }
+    componentWillMount(){
+        if(this.Auth.loggedIn())
+            this.props.history.replace('/');
+    }
+    render() {
+        return (
+            <div className="center">
+                <Container>
+                    <Row className={this.state.globalError === '' ? 'd-none' : ''}>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
+                            <Alert color="danger">
+                                {this.state.globalError}
+                            </Alert>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
+                            <Card body outline color="secondary" width="50%">
+                                <CardTitle className="text-center">Login</CardTitle>
+                                <CardText>
+                                    <form onSubmit={this.handleFormSubmit}>
+                                        <FormGroup>
+                                            <Input
+                                                className="form-control"
+                                                placeholder="E-mail"
+                                                value={this.state.email}
+                                                name="email"
+                                                type="email"
+                                                onChange={(event) => this.handleUserInput(event)}
+                                                valid={this.state.emailValid}
+                                                invalid={!this.state.emailValid}
+                                            />
+                                            <FormFeedback invalid>
+                                                El e-mail no es correcto
+                                            </FormFeedback>
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Input
+                                                className="form-control"
+                                                placeholder="Contraseña"
+                                                value={this.state.password}
+                                                name="password"
+                                                type="password"
+                                                valid={!this.state.passwordValid}
+                                                invalid={!this.state.passwordValid}
+                                                onChange={(event) => this.handleUserInput(event)}
+                                            />
+                                            <FormFeedback invalid>
+                                                El password debe tener al menos 2 caracteres
+                                            </FormFeedback>
+                                        </FormGroup>
+                                        <FormGroup className="text-center">
+                                            <Button type="submit" color="primary" disabled={!this.state.formValid}>Ingresar</Button>{' '}
+                                        </FormGroup>
+                                    </form>
+                                </CardText>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+
+    handleFormSubmit(e){
+        e.preventDefault();
+        if (!this.state.formValid) {
+            return;
+        }
+        this.Auth.login(this.state.email,this.state.password)
+            .then(res =>{
+               this.props.history.replace('/');
+            })
+            .catch(err =>{
+                this.setState({
+                    globalError: err
+                });
+            })
+    }
+
+
+    handleUserInput (e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value},
+            () => { this.validateField(name, value) });
+    }
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
+
+        switch(fieldName) {
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.email = emailValid ? '' : ' es inválido';
+                break;
+            case 'password':
+                passwordValid = value.length >= 2;
+                fieldValidationErrors.password = passwordValid ? '': ' es muy corto';
+                break;
+            default:
+                break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+            emailValid: emailValid,
+            passwordValid: passwordValid
+        }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    }
+
+    validationStatus(error) {
+        return(error.length === 0 ? 'valid' : 'invalid');
+    }
+}
+
+export default Login;
